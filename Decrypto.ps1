54bf3b6852e5fa53ea892136d6d71b0fa9a3dd22
tntrock <a940125@gmail.com>
2025-02-08 10:54:31 +0800

    Create Decrypto.ps1

---
+++ Decrypto.ps1
@@ -0,0 +1,12 @@
+$Password = "P@ssW0rd"
+$Key = (New-Object System.Text.UTF8Encoding).GetBytes($Password.PadRight(32, "X").Substring(0,32))
+$IV = New-Object Byte[] 16
+$AES = New-Object System.Security.Cryptography.AesManaged
+$AES.Key = $Key
+$AES.IV = $IV
+$Decryptor = $AES.CreateDecryptor()
+$InputFile = "Input_File_Path"
+$OutputFile = "Output_File_Path"
+$FileBytes = [System.IO.File]::ReadAllBytes($InputFile)
+$DecryptedBytes = $Decryptor.TransformFinalBlock($FileBytes, 0, $FileBytes.Length)
+[System.IO.File]::WriteAllBytes($OutputFile, $DecryptedBytes)
