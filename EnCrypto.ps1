02f41e66708e860f86a61a7df734ce85e6af5a1f
tntrock <a940125@gmail.com>
2025-02-08 10:53:06 +0800

    Create EnCrypto.ps1
    加密

---
+++ EnCrypto.ps1
@@ -0,0 +1,10 @@
+$Password = "P@ssW0rd"
+$Key = (New-Object System.Text.UTF8Encoding).GetBytes($Password.PadRight(32, "X").Substring(0,32))
+$IV = New-Object Byte[] 16
+$AES = New-Object System.Security.Cryptography.AesManaged
+$AES.Key = $Key
+$AES.IV = $IV
+$Encryptor = $AES.CreateEncryptor()
+$InputFile = "Input_File_Path"
+$OutputFile = Input_File_Path"
+$FileBytes = [System.IO.File]::ReadAllBytes($InputFile)
